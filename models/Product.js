e783be3ec881f1b13416331b58fb585b8ab7bc3a
Vinay Kumar <146341986+VinayKumar1510@users.noreply.github.com>
2025-09-08 14:25:56 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,3 +1,4 @@
+// models/Product.js
 import mongoose from "mongoose";
 
 const ProductSchema = new mongoose.Schema(
@@ -5,6 +6,7 @@ const ProductSchema = new mongoose.Schema(
     name: { type: String, required: true },
     price: { type: Number, required: true },
     category: { type: String, required: true },
+    image: { type: String, required: true },
   },
   { timestamps: true }
 );
